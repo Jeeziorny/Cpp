80164306abe76db30f4af2744820126031e3fe32
kantj <kamij98@gmail.com>
2019-10-08 16:52:47 +0200

    Done

--- StringMatching/StringMatching.cpp
+++ StringMatching/StringMatching.cpp
@@ -4,37 +4,75 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <array>
+#include <string>
 
-std::map<char, std::vector<int>>
-FA::compute_transition_function(std::string pattern, std::set<char> alphabet);
-bool FA::isSuffix(std::string suffix, std::string pattern);
-void FA::finite_automaton_matcher(std::string text, std::map<char, std::vector<int>> traFunc, int patLen);
+namespace FA
+{
+  std::map<char, std::vector<int>>
+  compute_transition_function(std::string pattern, std::set<char> alphabet);
+  bool isSuffix(std::string suffix, std::string pattern);
+  std::set<char> charsIn(std::string pattern);
+  void finite_automaton_matcher(std::string pattern, std::string text);
+}
+
+namespace KMP
+{
+  int* compute_prefix_function(const std::string text);
+  void kmp_matcher(const std::string p, const std::string t);
+}
 
 int main()
 {
-	char sigma[] = { 'a', 'b', 'c' };
-	std::set<char> alphabet (sigma, sigma+3);
-	std::string pattern = "ababaca";
-	std::string text = "ababacababacasdababaca";
-	auto result = FA::compute_transition_function(pattern, alphabet);
-	FA::finite_automaton_matcher(text, result, pattern.length());
+  std::string pattern, text, choice;
+  std::cout << "Pattern: ";
+  getline(std::cin, pattern);
+  std::cout << "Text: ";
+  getline(std::cin, text);
+  std::cout << "F for string matching with finite automaton" << std::endl;
+  std::cout << "K for string matching with knuth-morris-pratt algorithm" << std::endl;
+  getline(std::cin, choice);
+  if (choice.compare("K") == 0)
+    KMP::kmp_matcher(pattern, text);
+  else if (choice.compare("F") == 0)
+    FA::finite_automaton_matcher(pattern, text);
+  else
+    std::cout << "Incorrect parameter, bye" << std::endl;
+  return 0;
 }
 
 namespace FA {
-	void finite_automaton_matcher(std::string text, std::map<char, std::vector<int>> traFunc, int patLen)
+	void finite_automaton_matcher(std::string pattern, std::string text)
 	{
+    int patLen = pattern.length();
+    std::set<char> alphabet = charsIn(pattern);
+
+    auto traFunc = compute_transition_function(pattern, alphabet);
+
 		int n = text.length();
 		int q = 0;
-		for (int i = 1; i < n; i++)
+		for (int i = 0; i < n; i++)
 		{
 			if (traFunc.count(text[i]) != 0)
 			{
 				q = traFunc[text[i]][q];
 				if (q == patLen)
-					std::cout << "Pattern occurs with shift " << i - patLen << std::endl;
-			}
+					std::cout << "Pattern occurs with shift " << i - patLen + 1 << std::endl;
+			} 
+      else
+      {
+        q = 0;
+      }
 		}
 	}
+
+  std::set<char> charsIn(std::string pattern)
+  {
+    std::set<char> result = std::set<char>();
+    for (char c : pattern)
+      result.insert(c);
+    return result;
+  }
 
 	std::map<char, std::vector<int>>
 		compute_transition_function(std::string pattern, std::set<char> alphabet)
@@ -66,3 +104,42 @@ namespace FA {
 	}
 }
 
+namespace KMP
+{
+  void kmp_matcher(const std::string p, const std::string t)
+  {
+    int n = t.length();
+    int m = p.length() - 1;
+    int * pi = compute_prefix_function(p);
+    int q = -1;
+    for (int i = 0; i < n; i++)
+    {
+      while (q > -1 && p[q + 1] != t[i])
+        q = pi[q];
+      if (p[q + 1] == t[i])
+        q++;
+      if (q == m)
+      {
+        std::cout << "Pattern occurs with shift " << i - m;
+        q = pi[q];
+      }
+    }
+  }
+
+  int* compute_prefix_function(const std::string text)
+  {
+    int m = text.length();
+    int * result = new int[m];
+    result[0] = -1;
+    int k = -1;
+    for (int q = 1; q < m; q++)
+    {
+      while (k > -1 && text[k + 1] != text[q])
+        k = result[k];
+      if (text[k + 1] == text[q])
+        k++;
+      result[q] = k;
+    }
+    return result;
+  }
+}
